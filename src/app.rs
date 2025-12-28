1c8ee62fadf8f6e6a73ca5f8a96b08cb3180d927
Avyukt27 <avyukt.aggarwal007@gmail.com>
2025-12-28 14:27:30 +0530

    Created window

---
+++ src/app.rs
@@ -0,0 +1,48 @@
+use std::sync::Arc;
+
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::ActiveEventLoop;
+use winit::window::{Window, WindowId};
+
+pub struct App {
+    window: Option<Arc<Window>>,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self { window: None }
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = event_loop
+            .create_window(
+                Window::default_attributes()
+                    .with_title("Sprite Renderer")
+                    .with_inner_size(winit::dpi::LogicalSize::new(600.0, 600.0)),
+            )
+            .unwrap();
+
+        let window = Arc::new(window);
+        self.window = Some(window);
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => {}
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use winit::event_loop::{ControlFlow, EventLoop};
+
+mod app;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = app::App::new();
+    event_loop.run_app(&mut app).unwrap();
 }
